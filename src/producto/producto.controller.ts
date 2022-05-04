06c54e6eca8c6804a833d47dcf235849fc555816
guilherme1313 <guilhermehudsonfreitas13@gmail.com>
2022-05-04 14:58:19 -0300

    autenticando-API

--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -1,33 +1,37 @@
+import { JwtAuthGuard } from './../auth/shared/jwt.auth.guard';
 import { ProductoDto } from './dto/producto.dto';
 import { ProductoService } from './producto.service';
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
 
 @Controller('products')
 export class ProductoController {
 
     constructor(private readonly productoService: ProductoService){}
-
+    @UseGuards(JwtAuthGuard)
     @Get()
     async getAll(){
         return await this.productoService.getAll();
     }
-
+    @UseGuards(JwtAuthGuard)
     @Get(':id')
     async getOne(@Param('id', ParseIntPipe) id:number){
         return await this.productoService.findById(id);
     }
-
+    
+    @UseGuards(JwtAuthGuard)
     @UsePipes(new ValidationPipe({whitelist: true}))
     @Post()
     async create(@Body() dto:ProductoDto){
         return await this.productoService.create(dto);
     }
+
+    @UseGuards(JwtAuthGuard)
     @UsePipes(new ValidationPipe({whitelist: true}))
     @Put(':id')
     async update(@Param('id', ParseIntPipe) id:number, @Body() dto:ProductoDto){
         return await this.productoService.update(id,dto);
     }
-
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     async delete(@Param('id', ParseIntPipe) id:number){
         return await this.productoService.delete(id);
